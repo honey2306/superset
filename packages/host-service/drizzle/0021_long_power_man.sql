7d4463966acc6742e45cee6e00916dd6934cdc8d
wufan17 <wufan17@kuaishou.corp.com>
2026-08-24 09:18:31 +0800

    chore(host-service): add delegation context migration

---
+++ packages/host-service/drizzle/0021_long_power_man.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `delegation_runs` ADD `profile_id` text;--> statement-breakpoint
+ALTER TABLE `delegation_runs` ADD `context_snapshot_json` text;--> statement-breakpoint
+ALTER TABLE `delegation_runs` ADD `result_json` text;
